23c840aa3cde17a46dc8e58e233e74524864e4e7
Loki Ng <dev@lokikl.com>
2022-04-14 18:44:27 +0800

    updated about icons

--- install.sh
+++ install.sh
@@ -12,6 +12,14 @@
 # Please pay extra attention to nvim.d/my/keymap.lua, some builtin keymaps are cancelled on purpose.
 # Please fork this repo, modify to fit your habit.
 
+# Icons
+# access https://www.nerdfonts.com/font-downloads
+# download and unzip the one you like (I use MPlus Nerd Font)
+# mv 'M+ 1mn light Nerd Font Complete Mono.ttf' ~/.local/share/fonts
+# fc-cache -fv
+# fc-list :mono | awk -F: '{print $2}' | sort -u
+# change gnome-terminal profile to use mplus (size = 17, make sure the font looks taller)
+
 ln -s `pwd`/vimrc.lua ~/.config/nvim/init.lua
 ln -s `pwd`/nvim.d ~/.config/nvim/lua
 ln -s `pwd`/UltiSnips ~/.config/nvim/UltiSnips
