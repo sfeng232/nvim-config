e471da0ba946dd6c50fa627aed79ea7781963b2c
Loki Ng <dev@lokikl.com>
2022-04-21 23:46:46 +0800

    auto reload tmux on install

--- install.sh
+++ install.sh
@@ -11,10 +11,14 @@ ln -s `pwd`/nvim.d ~/.config/nvim/lua
 ln -s `pwd`/UltiSnips ~/.config/nvim/UltiSnips
 ln -s `pwd`/tmux.conf ~/.tmux.conf
 
+echo "Symlinks done"
+
 if ! which open-project 1>/dev/null 2>&1; then
   sourceLine="export PATH=\"\$PATH:`pwd`/bin\""
   [ -f ~/.bashrc ] && echo $sourceLine >> ~/.bashrc
   [ -f ~/.zshrc ] && echo $sourceLine >> ~/.zshrc
-else
-  echo "Done"
+  echo "RC files Done"
 fi
+
+tmux source-file ~/.tmux.conf
+echo "Reload tmux done"
