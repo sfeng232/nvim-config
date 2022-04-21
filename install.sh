5fbeab643d0e6a0fb0b818ec2685d7a1da1d6c07
Loki Ng <dev@lokikl.com>
2022-04-21 23:37:39 +0800

    moved in tmux related config

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+rm -rf \
+  ~/.config/nvim/init.lua \
+  ~/.config/nvim/lua \
+  ~/.config/nvim/UltiSnips \
+  ~/.tmux.conf
+
 ln -s `pwd`/vimrc.lua ~/.config/nvim/init.lua
 ln -s `pwd`/nvim.d ~/.config/nvim/lua
 ln -s `pwd`/UltiSnips ~/.config/nvim/UltiSnips
+ln -s `pwd`/tmux.conf ~/.tmux.conf
+
+if ! which open-project 1>/dev/null 2>&1; then
+  sourceLine="export PATH=\"\$PATH:`pwd`/bin\""
+  [ -f ~/.bashrc ] && echo $sourceLine >> ~/.bashrc
+  [ -f ~/.zshrc ] && echo $sourceLine >> ~/.zshrc
+else
+  echo "Done"
+fi
