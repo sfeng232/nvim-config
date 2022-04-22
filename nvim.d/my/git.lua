d5713ff28ce1f00a72a804cd8a8256840b9559be
Loki Ng <dev@lokikl.com>
2022-04-22 17:28:15 +0800

    updated git related keyboard shortcuts

--- nvim.d/my/git.lua
+++ nvim.d/my/git.lua
@@ -46,33 +46,31 @@ gitsigns.setup {
     enable = false,
   },
   on_attach = function(bufnr)
-    local function map(mode, lhs, rhs, opts)
-      opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
-      vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+    local gs = package.loaded.gitsigns
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
     end
-    map('n', 'gj', '<cmd>Gitsigns next_hunk<cr>')
-    map('n', 'gk', '<cmd>Gitsigns prev_hunk<cr>')
-    map('n', 'gr', '<cmd>Gitsigns reset_hunk<cr>')
-    -- stage / reset hunk with whichkey, basically <leader>g + s/u/r
+    map('n', 'gj', gs.next_hunk)
+    map('n', 'gk', gs.prev_hunk)
+    map('n', 'gr', gs.reset_hunk)
+    map('n', 'gp', gs.preview_hunk)
+    map('n', 'gd', gs.diffthis)
+    -- stage/unstage hunk with <leader>g + s/u
+    map("n", "<leader>gk", ":call system('gitk ' . expand('%'))<cr>")    -- only the current file
+    map("n", "<leader>gkk", ":call system('gitk')<cr>")                  -- the whole repo
   end
 }
 
 local opts = { noremap = true, silent = true }
 local map = vim.api.nvim_set_keymap
-map("n", "gh", ":call system('gitk')<cr>", opts)
-map("n", "gH", ":call system('gitk ' . expand('%'))<cr>", opts)
 map("n", "gs", "<cmd>Telescope git_status<cr>", opts)
 map("n", "gu", "<cmd>call TmuxPopup('echo git pull...; git pull; any-key')<cr>", opts)
-map("n", "gd", "<cmd>call GitDiff()<cr>", opts)
 map("n", "gi", "<cmd>call GitCommit()<cr>", opts)
 vim.cmd [[
   function! TmuxPopup(cmd)
     call system("tmux popup -E \"zsh -c 'cd `pwd`; pwd;" . a:cmd . "'\"")
-  endfunction
-
-  function! GitDiff()
-    let cmd = 'git diff --color | diff-so-fancy | less'
-    call system("tmux splitw \"zsh -c '" . cmd . "'\"")
   endfunction
 
   function! GitCommit()
