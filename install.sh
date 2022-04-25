4479f4a244843a43bd95e2e42be9e80b4a4ac0b0
Loki Ng <dev@lokikl.com>
2022-04-25 12:51:43 +0800

    added kitty config (gpu-based terminal emulator)

--- install.sh
+++ install.sh
@@ -34,12 +34,14 @@ rm -rf \
   ~/.config/nvim/init.lua \
   ~/.config/nvim/lua \
   ~/.config/nvim/UltiSnips \
-  ~/.tmux.conf
+  ~/.tmux.conf \
+  ~/.config/kitty/kitty.conf
 
 ln -s `pwd`/vimrc.lua ~/.config/nvim/init.lua
 ln -s `pwd`/nvim.d ~/.config/nvim/lua
 ln -s `pwd`/UltiSnips ~/.config/nvim/UltiSnips
 ln -s `pwd`/tmux.conf ~/.tmux.conf
+ln -s `pwd`/kitty.conf ~/.config/kitty/kitty.conf
 
 echo "Symlinks done"
 
