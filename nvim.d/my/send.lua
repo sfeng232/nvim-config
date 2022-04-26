dede41e69526f7c70f806677f0fe2b9349c8c215
Loki Ng <dev@lokikl.com>
2022-04-26 23:53:41 +0800

    bug: failed to use ss in visual mode bug: failed to send cmd with single quote with ss

--- nvim.d/my/send.lua
+++ nvim.d/my/send.lua
@@ -25,7 +25,7 @@ M.send_line_to_next_pane = function(line)
     return
   end
   last_line = line
-  local cmd = "tmux send-keys -t " .. next_pane .. " '" .. line .. "' enter"
+  local cmd = "tmux send-keys -t" .. next_pane .. " '" .. line:gsub("'", [['\'']]) .. "' enter"
   M.run_shell(cmd)
 end
 
@@ -43,11 +43,9 @@ M.send_last_line = function()
 end
 
 M.send_highlighted_lines = function()
-  local r1 = unpack(vim.api.nvim_buf_get_mark(0, "<")) - 1
-  local r2 = unpack(vim.api.nvim_buf_get_mark(0, ">"))
-  local text = vim.api.nvim_buf_get_lines(0, r1, r2, false)
-  local line = table.concat(text, "\n")
-  M.send_line_to_next_pane(line)
+  vim.cmd([[normal "ay]])
+  local cmd = vim.fn.getreg("a")
+  M.send_line_to_next_pane(cmd)
 end
 
 M.run_popup = function(cmd)
