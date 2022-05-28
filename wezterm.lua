7090866f817ddc8d074636bf0410f3ba99d8f254
Loki Ng <dev@lokikl.com>
2022-05-28 19:12:23 +0800

    feat: updated wezterm config for larger line height

--- wezterm.lua
+++ wezterm.lua
@@ -5,6 +5,10 @@ return {
   font = wezterm.font("Fantasque Sans Mono", {weight="Regular"}),
   -- color_scheme = "Solarized Dark - Patched",
   -- color_scheme = "Batman",
-  line_height = 1.2,
+  line_height = 1.3,
   hide_tab_bar_if_only_one_tab = true,
+  use_ime = false,
+
+  -- WhenFollowedBySpace (default), Never, Always
+  -- allow_square_glyphs_to_overflow_width = "Never",
 }
