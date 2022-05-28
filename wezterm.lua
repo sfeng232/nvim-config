0125c81f136ec54abe18af25053f02320511b2f1
Loki Ng <dev@lokikl.com>
2022-05-28 12:59:09 +0800

    feat: adding wezterm

---
+++ wezterm.lua
@@ -0,0 +1,8 @@
+-- ~/.config/wezterm/wezterm.lua
+local wezterm = require 'wezterm';
+return {
+  font_size = 15.0,
+  font = wezterm.font("Fantasque Sans Mono", {weight="Regular"}),
+  color_scheme = "Batman",
+  line_height = 1.2,
+}
