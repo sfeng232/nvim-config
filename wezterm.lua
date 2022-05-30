a3b0e78863cc03976cfd2c806042a7e69173e92f
Loki Ng <dev@lokikl.com>
2022-05-30 16:07:17 +0800

    feat: fine tuned wezterm font size and line height

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,14 @@
 -- ~/.config/wezterm/wezterm.lua
 local wezterm = require 'wezterm';
 return {
-  font_size = 17.0,
+  font_size = 19.0,
   -- ctrl+- and ctrl+= to change
   -- ctrl+0 to reset
   --
   font = wezterm.font("Fantasque Sans Mono", {weight="Regular"}),
   -- color_scheme = "Solarized Dark - Patched",
   -- color_scheme = "Batman",
-  line_height = 1.3,
+  line_height = 1.1,
   hide_tab_bar_if_only_one_tab = true,
   use_ime = false,
 
