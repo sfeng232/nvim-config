03a740d0d8a02cfeab98596594836b52fe3c96ae
Loki Ng <dev@lokikl.com>
2022-07-27 16:20:11 +0800

    fix: fixed settings for the latest nvim-tree

--- nvim.d/my/explorer.lua
+++ nvim.d/my/explorer.lua
@@ -1,26 +1,3 @@
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
-
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
@@ -28,7 +5,7 @@ end
 
 local lib = require("nvim-tree.lib")
 local view = require("nvim-tree.view")
-local open_file = require('nvim-tree.actions.open-file')
+local open_file = require('nvim-tree.actions.node.open-file')
 
 nvim_tree.setup {
   auto_reload_on_write = true,
