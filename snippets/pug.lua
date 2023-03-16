a20d0648b5eae58d30f5fff50ca29dc45d5b4df2
Loki Ng <dev@lokikl.com>
2023-03-16 22:37:48 +0800

    feat: enhanced the i18n snippet

--- snippets/pug.lua
+++ snippets/pug.lua
@@ -2,5 +2,9 @@ return {
   s("jt", f(function(args, snip)
     local t = table.concat(snip.env.TM_SELECTED_TEXT, "")
     return "t`" .. t:sub(2, -2) .. "`"
+  end, {})),
+  s("=t", f(function(args, snip)
+    local t = table.concat(snip.env.TM_SELECTED_TEXT, "")
+    return "= t`" .. t:sub(2, -1) .. "`"
   end, {}))
 }
