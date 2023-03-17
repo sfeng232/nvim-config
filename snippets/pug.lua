41f4c999a8218b64c6ae711e97198d574e980833
Loki Ng <dev@lokikl.com>
2023-03-17 11:16:04 +0800

    chore: more i18n snip

--- snippets/pug.lua
+++ snippets/pug.lua
@@ -3,6 +3,10 @@ return {
     local t = table.concat(snip.env.TM_SELECTED_TEXT, "")
     return "t`" .. t:sub(2, -2) .. "`"
   end, {})),
+  s("tt", f(function(args, snip)
+    local t = table.concat(snip.env.TM_SELECTED_TEXT, "")
+    return "#{t`" .. t .. "`}"
+  end, {})),
   s("=t", f(function(args, snip)
     local t = table.concat(snip.env.TM_SELECTED_TEXT, "")
     return "= t`" .. t:sub(1, -1) .. "`"
