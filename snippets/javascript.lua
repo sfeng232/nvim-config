7412acb5d715a7e907bd46c44a1b2fe9345fe2de
Loki Ng <dev@lokikl.com>
2024-04-10 10:10:24 +0800

    feat: added a random label to cl logging snippet

--- snippets/javascript.lua
+++ snippets/javascript.lua
@@ -1,6 +1,26 @@
+math.randomseed(os.time())
+
+function randomString(length)
+	local res = ""
+	for i = 1, length do
+		res = res .. string.char(math.random(97, 122))
+	end
+	return res
+end
+
 return {
   s("t", f(function(args, snip)
     local t = table.concat(snip.env.TM_SELECTED_TEXT, "")
     return "t`" .. t:sub(2, -2) .. "`"
-  end, {}))
+  end, {})),
+
+  s("cl", {
+    t("console.log(`"),
+    f(function()
+      return randomString(4)
+    end, {}),
+    t(": "),
+    i(1),
+    t("`);"),
+  })
 }
