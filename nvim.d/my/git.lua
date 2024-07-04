bcbdc36dbd032a0b19a0ad704d62eb697133aa50
Loki Ng <dev@lokikl.com>
2024-07-05 00:22:03 +0800

    feat: fixed gitsigns after pull

--- nvim.d/my/git.lua
+++ nvim.d/my/git.lua
@@ -3,67 +3,41 @@ if not status_ok then
   return
 end
 
--- gitsigns.setup {
---   -- signs = {
---   --   add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
---   --   change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
---   --   delete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
---   --   topdelete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
---   --   changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
---   -- },
---   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
---   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
---   linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
---   word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
---   watch_gitdir = {
---     interval = 1000,
---     follow_files = true,
---   },
---   attach_to_untracked = true,
---   -- current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
---   -- current_line_blame_opts = {
---   --   virt_text = true,
---   --   virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
---   --   delay = 1000,
---   --   ignore_whitespace = false,
---   -- },
---   -- current_line_blame_formatter_opts = {
---   --   relative_time = false,
---   -- },
---   sign_priority = 6,
---   update_debounce = 100,
---   status_formatter = nil, -- Use default
---   max_file_length = 40000,
---   preview_config = {
---     -- Options passed to nvim_open_win
---     border = "single",
---     style = "minimal",
---     relative = "cursor",
---     row = 0,
---     col = 1,
---   },
---   -- yadm = {
---   --   enable = false,
---   -- },
---   on_attach = function(bufnr)
---     local gs = package.loaded.gitsigns
---     local function map(mode, l, r, opts)
---       opts = opts or {}
---       opts.buffer = bufnr
---       vim.keymap.set(mode, l, r, opts)
---     end
---     map('n', 'gj', gs.next_hunk)
---     map('n', 'gk', gs.prev_hunk)
---     map('n', 'gr', gs.reset_hunk)
---     map('n', 'gh', gs.preview_hunk)
---     --[[ map('n', 'gd', gs.diffthis) ]]
---     map('n', 'gd', ":call system('git difftool -t diffuse -y -- ' . expand('%'))<cr>")
---     map('n', 'gt', ":call system('gitk')<cr>")
---     -- stage/unstage hunk with <leader>g + s/u
---     map("n", "<leader>gk", ":call system('gitk ' . expand('%'))<cr>")    -- only the current file
---     map("n", "<leader>gkk", ":call system('gitk')<cr>")                  -- the whole repo
---   end
--- }
+gitsigns.setup {
+  sign_priority = 6,
+  update_debounce = 100,
+  max_file_length = 5000, -- Disable if file is longer than this (in lines)
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+    map('n', 'gj', gs.next_hunk)
+    map('n', 'gk', gs.prev_hunk)
+    map('n', 'gr', gs.reset_hunk)
+    map('n', 'gh', gs.preview_hunk)
+    --[[ map('n', 'gd', gs.diffthis) ]]
+    map('n', 'gd', ":call system('git difftool -t diffuse -y -- ' . expand('%'))<cr>")
+    map('n', 'gt', ":call system('gitk')<cr>")
+    -- stage/unstage hunk with <leader>g + s/u
+    map("n", "<leader>gk", ":call system('gitk ' . expand('%'))<cr>")    -- only the current file
+    map("n", "<leader>gkk", ":call system('gitk')<cr>")                  -- the whole repo
+  end
+  --   watch_gitdir = {
+  --     interval = 1000,
+  --     follow_files = true,
+  --   },
+}
 
 local opts = { noremap = true, silent = true }
 local map = vim.api.nvim_set_keymap
